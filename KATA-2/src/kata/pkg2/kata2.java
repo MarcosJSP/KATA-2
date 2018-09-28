78cbcdb55b8c041554283a47a22a69742c41805f
MarcosJSP <marcos.academia.2@gmail.com>
2018-09-28 15:04:33 +0200

    V 1.0

---
+++ KATA-2/src/kata/pkg2/kata2.java
@@ -0,0 +1,55 @@
+package kata.pkg2;
+
+import com.sun.prism.impl.PrismSettings;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class kata2 {
+    private final Random rand = new Random(System.currentTimeMillis());
+    private Map <Integer, Integer> histogram = new HashMap<>();
+    private final int[] data = new int[15];
+    
+    public static void main(String[] args) {
+        kata2 i = new kata2();
+        i.control(); 
+    }
+    
+    public void control (){
+        chargingArray();
+        chargingHash();
+        showingContent();
+    }
+    
+    public void  chargingArray(){
+        for(int i= 0; i < data.length; i++){
+            data[i] = ThreadLocalRandom.current().nextInt(-10, 11);
+        }
+    }
+    
+    public void chargingHash(){
+        for(int i = 0; i < data.length; i++){
+            if(histogram.containsKey(data[i])){
+                histogram.put(data[i], histogram.get(data[i]) + 1);
+            }else{
+                histogram.put(data[i], 1);
+            }
+        }
+    }
+    
+    public void showingContent(){        
+        System.out.println("-Array-");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println("\n-Histogram-");
+        Iterator it = histogram.entrySet().iterator();
+        
+        for(Map.Entry<Integer,Integer> entry: histogram.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+    }
+}
