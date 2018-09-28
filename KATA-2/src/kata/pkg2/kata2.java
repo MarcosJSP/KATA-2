088cd28572b331338988900f9380184f8544f868
MarcosJSP <marcos.academia.2@gmail.com>
2018-09-28 15:13:57 +0200

    V 2.0

--- KATA-2/src/kata/pkg2/kata2.java
+++ KATA-2/src/kata/pkg2/kata2.java
@@ -14,7 +14,7 @@ public class kata2 {
     
     public static void main(String[] args) {
         kata2 i = new kata2();
-        i.control(); 
+        i.control();     
     }
     
     public void control (){
@@ -30,12 +30,8 @@ public class kata2 {
     }
     
     public void chargingHash(){
-        for(int i = 0; i < data.length; i++){
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+        for (int i : data) {
+            histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1: 1);
         }
     }
     
